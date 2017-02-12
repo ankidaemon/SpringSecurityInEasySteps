f4e9bf75de93ebb56bfea25f7a6cab20841fd34c
Ankit Mishra <ankitmishra1701@gmail.com>
2017-02-12 19:40:39 +0530

    Section 2 - Web Security Java Configuration

---
+++ SimpleSpringSecurity/src/main/java/com/demo/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+     
+/*   @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("ankidaemon").password("password").roles("USER")
+                .and().withUser("test").password("test").roles("USER");
+    }*/
+     
+    @Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("ankidaemon").password("password").roles("USER").build());
+		manager.createUser(User.withUsername("test").password("test").roles("USER").build());
+		return manager;
+	}
+}
