df4611913887366bd7da2e3e7d945df5934277ab
Ankit Mishra <ankitmishra1701@gmail.com>
2017-03-05 08:44:42 +0530

    cleanup

--- Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/SecurityWebAppInitializer.java
+++ Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/SecurityWebAppInitializer.java
@@ -2,6 +2,10 @@ package com.demo.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * @author ankidaemon
+ *
+ */
 public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
  
     public SecurityWebAppInitializer() {
