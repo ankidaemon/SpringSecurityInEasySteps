eb26f5aeb430ff0b81daa31b4187a13b8317facd
Ankit Mishra <ankitmishra1701@gmail.com>
2017-03-05 08:42:46 +0530

    cleanup

--- Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/CustomDigestAuthenticationEntryPoint.java
+++ Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/CustomDigestAuthenticationEntryPoint.java
@@ -1,9 +1,12 @@
 package com.demo.config;
 
-
 import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author ankidaemon
+ *
+ */
 @Component
 public class CustomDigestAuthenticationEntryPoint extends DigestAuthenticationEntryPoint{
     
