fa0aa710175233df38a3be609a90a194ac8adfc1
Ankit Mishra <ankitmishra1701@gmail.com>
2017-03-05 08:42:12 +0530

    cleanup

--- Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/CustomBasicAuthenticationEntryPoint.java
+++ Section3/Video_3.1/SpringSecurity-BasicAndDigestAuthentication/src/main/java/com/demo/config/CustomBasicAuthenticationEntryPoint.java
@@ -2,11 +2,9 @@ package com.demo.config;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
