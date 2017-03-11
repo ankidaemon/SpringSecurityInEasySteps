b9d9c33328754bf4cd732c1630bcac94e3f1b638
Ankit Mishra <ankitmishra1701@gmail.com>
2017-03-11 18:32:19 +0530

    initial

---
+++ Section4/Video_4.1/SpringSecurity-SecurityFilterChain/src/main/java/com/demo/config/WebAppIntializer.java
@@ -0,0 +1,42 @@
+package com.demo.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.support.*;
+
+/**
+ * @author ankidaemon
+ *
+ */
+public class WebAppIntializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		servletContext.addFilter(
+	            "customSpringSecurityFilterChain",
+	            new DelegatingFilterProxy("customSpringSecurityFilterChain"));
+	       
+		super.onStartup(servletContext);
+		
+	}
+}
